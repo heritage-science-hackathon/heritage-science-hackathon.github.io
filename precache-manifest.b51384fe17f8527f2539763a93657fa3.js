0b4691138b727d4fa66195933c0ef6f4876c031d
Rosie-Brigham <rosiebrigham@gmail.comm>
2019-03-12 21:51:29 +0000

    Updates

---
+++ precache-manifest.b51384fe17f8527f2539763a93657fa3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/about/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5e7b65462749e51da51f",
+    "url": "/about/static/js/runtime~main.c0e869d3.js"
+  },
+  {
+    "revision": "bb55a07389f3c0223bdb",
+    "url": "/about/static/js/main.6e9180ff.chunk.js"
+  },
+  {
+    "revision": "ae0b72b89335021e0997",
+    "url": "/about/static/js/2.4952e05c.chunk.js"
+  },
+  {
+    "revision": "bb55a07389f3c0223bdb",
+    "url": "/about/static/css/main.1b673d6a.chunk.css"
+  },
+  {
+    "revision": "6be737b6f5d3f46b0093df82811f0b68",
+    "url": "/about/index.html"
+  }
+];
