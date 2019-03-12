c24ccd2a5a1f50ab72077632d14a1da30bba5e42
Rosie-Brigham <rosiebrigham@gmail.comm>
2019-03-12 21:59:45 +0000

    quick fix

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="side-image fixed vh-100 w-50 dn-m overflow-hidden">
-          <img className="dn-m h-100 mw-none" src="../images/computer.jpeg" />
+          <img className="dn-m h-100 mw-none" src="./images/computer.jpeg" />
         </div>
         <div className="min-vh-100 ph4 w-50-l relative flex flex-column">
           <Nav />
