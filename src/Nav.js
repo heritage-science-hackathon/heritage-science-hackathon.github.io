6c188265d517d9b8b480a8c35449dab8cfadd7ff
Rosie-Brigham <rosiebrigham@gmail.comm>
2019-03-13 11:30:54 +0000

    adds some padding

--- src/Nav.js
+++ src/Nav.js
@@ -48,7 +48,7 @@ class Nav extends Component {
   }
   render () {
     return (
-      <nav className="pt3 pt4-ns mb4 mb0-ns">
+      <nav className="pt3 pt4-ns mb4 mb0-ns ph4">
         <ul className="list flex flex-wrap flex-nowrap-ns justify-between items-center pa0 ma0">
           {menu.map(item => <NavItem {...item} />)}
         </ul>
